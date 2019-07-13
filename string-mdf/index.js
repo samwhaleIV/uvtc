85b2fb95ff56c7eba42af3f356691ef4efa88925
Samuel Robinson <pinkstarsr11@gmail.com>
2019-07-13 14:42:03 -0700

    Added opponents into manifest linking

--- string-mdf/index.js
+++ string-mdf/index.js
@@ -6,8 +6,7 @@ const inputFolder = "../elven-uvtc/runtime/maps";
 const stringsFilePath = "../elven-uvtc/runtime/strings.js";
 const shadowStringsPath = "./shadow-strings.txt";
 const mapManifestPath = "../elven-uvtc/runtime/maps/manifest.js";
-
-const AUTO_STRING_PREFIX = "AUTO_";
+const opponentsFolder = "../elven-uvtc/runtime/battle/opponents";
 
 const walk = function(dir) {
     //https://stackoverflow.com/a/16684530
@@ -18,8 +17,10 @@ const walk = function(dir) {
         const stat = fs.statSync(file);
         if(stat && stat.isDirectory()) { 
             results = results.concat(walk(file));
-        } else { 
-            results.push(file);
+        } else {
+            if(!file.endsWith("manifest.js")) {
+                results.push(file);
+            }
         }
     });
     return results;
@@ -31,23 +32,35 @@ String.prototype.replaceAll = function(search,replacement) {
 
 const filesToReplaceStringsFrom = walk(inputFolder);
 filesToReplaceStringsFrom.push(scriptFile);
-console.log(walk(inputFolder));
 
 function createMapManifest() {
     function makeLine(fileName) {
         return `import ".${fileName}";`;
     }
     const lines = [];
+    console.log(filesToReplaceStringsFrom);
     filesToReplaceStringsFrom.forEach(fileName=>{
         if(fileName.startsWith(inputFolder)) {
             const name = fileName.split(inputFolder)[1];
-            if(name === "/manifest.js") {
-                return;
-            }
             lines.push(makeLine(name));
         }
     });
     fs.writeFileSync(mapManifestPath,lines.join("\r\n")+"\r\n");
+}
+function createOpponentManifest() {
+    function makeLine(fileName) {
+        return `import ".${fileName}";`;
+    }
+    const lines = [];
+    const files = walk(opponentsFolder);
+    console.log(files);
+    files.forEach(fileName=>{
+        if(fileName.startsWith(opponentsFolder)) {
+            const name = fileName.split(opponentsFolder)[1];
+            lines.push(makeLine(name));
+        }
+    });
+    fs.writeFileSync(opponentsFolder + "/manifest.js",lines.join("\r\n")+"\r\n");
 }
 
 function createShadowStringsFile() {
@@ -87,127 +100,7 @@ function createShadowStringsFile() {
     fs.writeFileSync(shadowStringsPath,Object.keys(strings).join("|"));
 }
 
-function reverseIDs() {
-    const strings = {};
-    let stringsFileLines = fs.readFileSync(stringsFilePath).toString().split("\r\n");
-
-    for(let i = 1;i<stringsFileLines.length;i++) {
-        const line = stringsFileLines[i].trim();
-        if(line === "}") {
-            break;
-        }
-        const key = line.split(":")[0];
-        let value = line.substring(key.length+1).trim();
-        if(value.endsWith(",")) {
-            value = value.substring(0,value.length-1);
-        }
-        strings[key] = value;
-    }
-
-    const IDs = Object.entries(strings).map(entry=>{
-        const pair = [];
-        pair[0] = `"${entry[0]}"`;
-        pair[1] = entry[1];
-        return pair;
-    });
-
-    for(let i = 0;i<filesToReplaceStringsFrom.length;i++) {
-        const fileName = filesToReplaceStringsFrom[i];
-        const startFileText = fs.readFileSync(fileName).toString();
-        let fileText = startFileText.replaceAll("\\n","\n");
-        
-        IDs.forEach(entry=>{
-            fileText = fileText.replaceAll(...entry)
-        });
-    
-        if(startFileText !== fileText) {
-            fs.writeFileSync(fileName,fileText);
-        }
-    }
-
-
-    console.log(strings);
-}
-
-function generateIDsInPlace() {
-    let stringsFileLines = fs.readFileSync(stringsFilePath).toString().split("\r\n");
-    let highestAutoID = 0;
-    let whereToInsertIntoStrings = 1;
-    for(let i = 1;i<stringsFileLines.length;i++) {
-        const line = stringsFileLines[i].trim().split(":")[0];
-        if(line === "}") {
-            whereToInsertIntoStrings = i;
-            break;
-        }
-        if(line.startsWith(AUTO_STRING_PREFIX)) {
-            const ID = Number(line.split(AUTO_STRING_PREFIX)[1]);
-            if(ID > highestAutoID) {
-                highestAutoID = ID;
-            }
-        }
-    }
-    
-    const newStrings = {};
-    
-    for(let i = 0;i<filesToReplaceStringsFrom.length;i++) {
-        const fileName = filesToReplaceStringsFrom[i];
-        const startFileText = fs.readFileSync(fileName).toString();
-        const fileText = startFileText.replaceAll("\\n","\n").split("");
-        let fileTextBuffer = "";
-    
-        let lastCharacterWasEscapeSequence = false;
-        let stringBuffer = "";
-        let inRegularString = false;
-        for(let i = 0;i<fileText.length;i++) {
-            const character = fileText[i];
-            if(stringBuffer !== "") {
-                if(character === ignoreStringType) {
-                    if(lastCharacterWasEscapeSequence) {
-                        stringBuffer += ignoreStringType;
-                    } else {
-                        stringBuffer += targetStringType;
-                        const tokenName = `${AUTO_STRING_PREFIX}${++highestAutoID}`;
-                        newStrings[tokenName] = stringBuffer.replaceAll("\n","\\n");
-                        fileTextBuffer += `"${tokenName}"`;
-                        stringBuffer = "";
-                    }
-                } else {
-                    if(character !== "\\") {
-                        stringBuffer += character;
-                    }
-                }
-            } else {
-                if(character === ignoreStringType && !inRegularString) {
-                    stringBuffer += targetStringType;
-                } else {
-                    fileTextBuffer += character;
-                }
-            }
-            lastCharacterWasEscapeSequence = character === "\\";
-            if(character === targetStringType) {
-                inRegularString = !inRegularString;
-            }
-        }
-    
-        if(startFileText !== fileTextBuffer) {
-            fs.writeFileSync(fileName,fileTextBuffer);
-        }
-    }
-    
-    const newStringEntries = Object.entries(newStrings);
-    let lastLine = stringsFileLines[whereToInsertIntoStrings-1] + ",";
-    stringsFileLines[whereToInsertIntoStrings-1] = lastLine;
-    if(newStringEntries.length) {
-        for(let i = 0;i<newStringEntries.length;i++) {
-            const entry = newStringEntries[i];
-            const shouldAddComma = i !== newStringEntries.length - 1;
-            stringsFileLines.splice(whereToInsertIntoStrings,0,`    ${entry[0]}: ${entry[1]}${shouldAddComma?",":""}`);
-            whereToInsertIntoStrings++;
-        }
-        fs.writeFileSync(stringsFilePath,stringsFileLines.join("\r\n"));
-    }
-}
-
 createShadowStringsFile();
 createMapManifest();
+createOpponentManifest();
 //opn("file:///C:/Users/jedisammy4/Documents/uvtc/string-baker.html");
