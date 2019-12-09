f7520b9abcb0cd7906c262af79289b491714fa9b
Samuel Robinson <pinkstarsr11@gmail.com>
2019-12-08 21:22:34 -0800

    Fix elves fill in performance once and for all

--- elven-uvtc/renderers/components/elves-fill-in.js
+++ elven-uvtc/renderers/components/elves-fill-in.js
@@ -1,11 +1,14 @@
 function ElvesFillIn() {
-    const pattern = context.createPattern(
-        imageDictionary["backgrounds/corrupt"],
-        "repeat"
+    const image = imageDictionary["backgrounds/corrupt"]
+    const offscreenCanvas = new OffscreenCanvas(image.width,image.height);
+    offscreenCanvas.getContext("2d").drawImage(
+        image,0,0,image.width,image.height
     );
+    const pattern = context.createPattern(offscreenCanvas,"repeat");
     this.render = () => {
+        context.rect(0,0,fullWidth,fullHeight);
         context.fillStyle = pattern;
-        context.fillRect(0,0,fullWidth,fullHeight);
+        context.fill();
     }
 }
 export default ElvesFillIn;
